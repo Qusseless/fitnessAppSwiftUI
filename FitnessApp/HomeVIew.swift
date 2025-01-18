aebb0469546c4676cd94ff7445479921322f04e4
Ilia Volkorez <ivolkore@mail.ru>
2025-01-18 14:33:03 +0100

    Adding activity circles

---
+++ FitnessApp/HomeVIew.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVIew.swift
+//  FitnessApp
+//
+//  Created by Ilya Volkorez on 2025-01-18.
+//
+
+import SwiftUI
+
+struct HomeVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeVIew()
+}
---
+++ FitnessApp/ProgressCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressCircleView.swift
+//  FitnessApp
+//
+//  Created by Ilya Volkorez on 2025-01-18.
+//
+
+import SwiftUI
+
+struct ProgressCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressCircleView()
+}
